3c8846a0728d31a9b358ad1fe4d5d7021b0ec44b
Matheu DeWitt <msdewitt@bsu.edu>
2017-02-22 13:26:46 -0500

    Add font increase and decrease options under menu.

--- js/project1_font_lang.js
+++ js/project1_font_lang.js
@@ -1,4 +1,7 @@
 // This file is the javascript for the fonts and languages submenu.
+
+// Font family
+
 $('#fontArial').click(function(){
   $('body').css("font-family", "Arial");
 });
@@ -7,4 +10,27 @@ $('#fontTimes').click(function(){
 });
 $('#fontCalibri').click(function(){
   $('body').css("font-family", "Calibri");
+});
+
+// Font size
+
+$('#fontSizeIncrease').click(function(){
+  var mySize = $('body').css("font-size");
+  var actualSize = parseFloat(mySize);
+  actualSize+=2;
+  var unit= mySize.slice(-2);
+  $('body').css("font-size", actualSize + unit);
+  $('button').css("font-size", actualSize + unit);
+  $('textarea').css("font-size", actualSize + unit);
+});
+
+
+$('#fontSizeDecrease').click(function(){
+  var mySize = $('body').css("font-size");
+  var actualSize = parseFloat(mySize);
+  actualSize-=2;
+  var unit= mySize.slice(-2);
+  $('body').css("font-size", actualSize + unit);
+  $('button').css("font-size", actualSize + unit);
+  $('textarea').css("font-size", actualSize + unit);
 });
