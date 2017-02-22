140deaa30318ffa262620b3e4f0daa828cfd7284
Matheu DeWitt <msdewitt@bsu.edu>
2017-02-22 13:15:02 -0500

    Add font opinions Arial, Times New Roman, and Calibri

--- js/project1_font_lang.js
+++ js/project1_font_lang.js
@@ -1,1 +1,10 @@
 // This file is the javascript for the fonts and languages submenu.
+$('#fontArial').click(function(){
+  $('body').css("font-family", "Arial");
+});
+$('#fontTimes').click(function(){
+  $('body').css("font-family", "TimesNewRoman");
+});
+$('#fontCalibri').click(function(){
+  $('body').css("font-family", "Calibri");
+});
